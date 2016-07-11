00055fa66f146861c882e074cce68cc38e295ed8
Santos Eliel <eliel.floyd@bol.com.br>
2016-07-11 11:31:36 -0300

    enviando alterações de hoje

---
+++ jogo.js
@@ -0,0 +1,40 @@
+var view = {
+  displayMessage: (msg) => {
+    var messageArea = document.getElementById("messageArea");
+    messageArea.innerHTML = msg;
+  },
+  displayHit: function(location){
+    var cell = document.getElementById(location);
+    cell.setAttribute("class","hit");
+  },
+  displayMiss: function(location){
+    var cell = document.getElementById(location);
+    cell.setAttribute("class","miss");
+  }
+};
+
+var model = {
+  boardSize: 7,
+  numShips: 3,
+  shipLength: 3,
+  shipsSunk: 0,
+
+  ships: [ {locations:["06","16","26"], hits:["","",""] },[]
+           {locations:["24","34","44"], hits:["","",""] },
+           {locations:["10","11","12"], hits:["","",""] } ],
+
+  fire: (guess) => {
+
+    for (var i = 0; i < this.numShips; i++) {
+      var ship = this.ships[i];
+      var index = ship.locations.indexOf(guess);
+      if(index >= 0) {
+        ship.hits[index] = "hit";
+        return true;
+      }
+    }
+    return false;
+
+  }
+
+};
