8f26a42c454337dcb7bd58846e765aaa3d01d56f
Santos Eliel <eliel.floyd@bol.com.br>
2016-07-11 11:43:52 -0300

    finalizando pela manhã

--- jogo.js
+++ jogo.js
@@ -19,22 +19,33 @@ var model = {
   shipLength: 3,
   shipsSunk: 0,
 
-  ships: [ {locations:["06","16","26"], hits:["","",""] },[]
+  ships: [ {locations:["06","16","26"], hits:["","",""] },
            {locations:["24","34","44"], hits:["","",""] },
            {locations:["10","11","12"], hits:["","",""] } ],
 
   fire: (guess) => {
-
     for (var i = 0; i < this.numShips; i++) {
       var ship = this.ships[i];
       var index = ship.locations.indexOf(guess);
       if(index >= 0) {
         ship.hits[index] = "hit";
+        if(this.isSunk(ship)) {
+          this.shipsSunk++;
+        }
         return true;
       }
     }
     return false;
 
+  },
+  isSunk: (ship) => {
+    for (var i = 0; i < this.shipLength; i++) {
+      if(ship.hits[i] !== "hit") {
+        return false;
+      }
+    }
+    return true;
   }
 
 };
+//347
