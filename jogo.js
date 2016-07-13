4d1f16a6c36d1382b80f20fa891a87df7b9aa3cb
Santos Eliel <eliel.floyd@bol.com.br>
2016-07-13 09:46:57 -0300

    finalizado

--- jogo.js
+++ jogo.js
@@ -19,9 +19,9 @@ var model = {
   shipLength: 3,
   shipsSunk: 0,
 
-  ships: [ {locations:["06","16","26"], hits:["","",""] },
-           {locations:["24","34","44"], hits:["","",""] },
-           {locations:["10","11","12"], hits:["","",""] } ],
+  ships: [ {locations:[0,0,0], hits:["","",""] },
+           {locations:[0,0,0], hits:["","",""] },
+           {locations:[0,0,0], hits:["","",""] } ],
 
   fire: function(guess){
     for (var i = 0; i < this.numShips; i++) {
@@ -50,6 +50,49 @@ var model = {
       }
     }
     return true;
+  },
+  generateShipLocations: function() {
+    var locations;
+    for(var i = 0; i < this.numShips; i++) {
+      do {
+        locations = this.generateShip();
+      } while(this.collision(locations));
+      this.ships[i].locations = locations;
+    }
+    console.log(locations);
+  },
+  generateShip: function() {
+    var direction = Math.floor(Math.random() * 2);
+    var row,col;
+
+    if(direction === 1) {
+      row = Math.floor(Math.random() * this.boardSize);
+      col = Math.floor(Math.random() * (this.boardSize - this.shipLength));
+    } else {
+      row = Math.floor(Math.random() * (this.boardSize - this.shipLength));
+      col = Math.floor(Math.random() * this.boardSize);
+    }
+
+    var newShipLocations = [];
+    for(var i = 0; i < this.shipLength; i++) {
+      if(direction === 1) {
+        newShipLocations.push(row+""+(col + i));
+      } else {
+        newShipLocations.push((row+1)+""+col);
+      }
+    }
+    return newShipLocations;
+  },
+  collision: function(locations) {
+    for(var i = 0; i < this.numShips; i++) {
+      var ship = model.ships[i];
+      for(var j = 0; j < locations.length; j++) {
+        if(ship.locations.indexOf(locations[j]) >= 0) {
+          return true;
+        }
+      }
+    }
+    return false;
   }
 
 };
@@ -95,6 +138,8 @@ function init() {
   fireButton.onclick = handleFireButton;
   var guessInput = document.getElementById("guessInput");
   guessInput.onkeypress = handleKeyPress;
+
+  model.generateShipLocations();
 }
 
 function handleFireButton() {
@@ -114,5 +159,3 @@ function handleKeyPress(e) {
 }
 
 window.onload = init;
-
-//364
